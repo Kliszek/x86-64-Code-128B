2cc0826cf989dab85619c8acffe1f0e6148380d8
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-01-15 19:04:41 +0100

    Added function load_codes for loading the file with codes

--- encode128.asm
+++ encode128.asm
@@ -7,6 +7,14 @@
 ;
 ;=====================================================================
 
+section .data
+DEST	dd 0
+BWTH	dd 0
+TEXT	dd 0
+CDFILE	db 'code128b.bin',0
+CODES times 856 db 0
+
+
 section	.text
 global	encode128
 
@@ -14,6 +22,31 @@ encode128:
 	push	ebp
 	mov	ebp, esp
 
-	mov	eax, 0			;return 0
+	mov eax, DWORD [ebp+8]
+	mov [DEST], DWORD eax
+	mov eax, DWORD [ebp+12]
+	mov [BWTH], DWORD eax
+	mov eax, DWORD [ebp+16]
+	mov [TEXT], DWORD eax
+
+
+	call load_codes
+	mov	eax, 0				;return 0
+
 	pop	ebp
+	ret
+
+load_codes:
+	mov eax, 5
+	mov ebx, CDFILE
+	mov ecx, 0				;O_RDONLY flag
+	mov edx, 0444o			;readable by everyone permissions
+	int 80h
+	
+	mov ebx, eax
+	mov eax, 3
+	mov ecx, CODES
+	mov edx, 855
+	int 80h
+
 	ret
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cstdio>
 
 extern "C" int encode128(unsigned char* dest_bitmap,
 	int bar_width,
@@ -7,10 +9,12 @@ extern "C" int encode128(unsigned char* dest_bitmap,
 int main(void)
 {
   char text[] = "Wind On The Hill";
-  unsigned char dest_bitmap[] = "output.bmp";
+  unsigned char* dest_bitmap;
   int bar_width = 1;
   int result;
-  
+
+  dest_bitmap = (unsigned char*)malloc(90054);
+
   printf("Input string      > %s\n", text);
 
   result = encode128(dest_bitmap, bar_width, text);
@@ -18,8 +22,22 @@ int main(void)
   switch(result)
   {
 	  case 0:
-		printf("Barcode successfully generated\n");
-		break;
+	  {
+		  printf("Barcode successfully generated\n");
+		  FILE* output = fopen("output.bmp", "w");
+		  if (!output)
+		  {
+			  printf("Error saving the file!\n");
+			  return 1;
+		  }
+		  fwrite(dest_bitmap, 1, 90054, output);
+		  fclose(output);
+		  break;
+	  }
+
+	  default:
+		  printf("Program returned this value: %i\n", result);
+		  break;
   }
   
   return result;
