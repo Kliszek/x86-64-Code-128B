a23aa4285b6cdda70d78d61ac9c88f58f06445e6
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-01-22 22:57:26 +0100

    Modified makefile and testing new registers

--- encode128.asm
+++ encode128.asm
@@ -1,89 +1,89 @@
-;=====================================================================
-; ECOAR - example Intel x86 assembly program
-;
-; Author:      Jakub Kliszko
-; Date:        2022-xx-xx
-; Description: Generate BMP file with Code 128B barcode of a given word
-;
-;=====================================================================
-
-section .data
-DEST	dd 0
-BWTH	dd 0
-TEXT	dd 0
-CDFILE	db 'code128b.bin',0
-CODES times 856 db 0
+#=====================================================================
+# ECOAR - example Intel x86 assembly program
+#
+# Author:      Jakub Kliszko
+# Date:        2022-xx-xx
+# Description: Generate BMP file with Code 128B barcode of a given word
+#
+#=====================================================================
 
 
-section	.text
-global	encode128
+		.text
+		.global	encode128
 
 encode128:
-	push ebp
+	push rbp
 	mov	ebp, esp
 
+#r8	dd 0
+#r9	dd 0
+#r10	dd 0
+#r11	db 'code128b.bin',0
+#r12 times 856 db 0
+
+
 	mov eax, DWORD [ebp+8]
-	mov [DEST], DWORD eax
+	mov [r8], DWORD eax
 	mov eax, DWORD [ebp+12]
-	mov [BWTH], DWORD eax
+	mov [r9], DWORD eax
 	mov eax, DWORD [ebp+16]
-	mov [TEXT], DWORD eax
+	mov [r10], DWORD eax
 
 
 	call analyze_input
-	call load_codes
+	call load_r12
 	call generate_header
 	call paint_white
 	mov edx, 30
 	mov ebx, 1
 
 	call generate_barcode	
-	mov	eax, 0			;return 0
+	mov	eax, 0			#return 0
 
 	mov ecx, 1
-exit:					;ecx indicates how many values should be popped
-	pop	ebp
+exit:					#ecx indicates how many values should be popped
+	pop	rbp
 	loop exit
 	ret
 
-load_codes:
-	push eax
-	push ebx
-	push ecx
-	push edx
+load_r12:
+	push rax
+	push rbx
+	push rcx
+	push rdx
 
-	mov eax, 5
-	mov ebx, CDFILE
-	mov ecx, 0				;O_RDONLY flag
-	mov edx, 0444o			;readable by everyone permissions
+	mov eax, 2
+	mov ebx, r11
+	mov ecx, 0				#O_RDONLY flag
+	mov edx, 0444o			#readable by everyone permissions
 	int 80h
 
-	mov ecx, 6				;popping 6 values in case of error
-	cmp eax, 0				;checking if there was an error opening the file
+	mov ecx, 6				#popping 6 values in case of error
+	cmp eax, 0				#checking if there was an error opening the file
 	mov eax, 3
 	jle exit
 	
 	mov ebx, eax
-	mov eax, 3
-	mov ecx, CODES
+	mov eax, 0
+	mov ecx, r12
 	mov edx, 855
 	int 80h
 
 	mov ecx, 6
-	cmp eax, 0				;checking if there was an error reading the file
+	cmp eax, 0				#checking if there was an error reading the file
 	mov eax, 4
 	jle exit
 
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
 	ret
 
 generate_header:
-	push eax
+	push rax
 
-	mov eax, [DEST]
+	mov eax, [r8]
 	mov [eax], WORD 0x4d42
 	mov [eax+2], DWORD 90054
 	mov [eax+6], DWORD 0
@@ -102,29 +102,29 @@ generate_header:
 	mov [eax+32], DWORD 0
 	mov [eax+36], DWORD 0
 
-	pop eax
+	pop rax
 	ret
 
 
 analyze_input:
-	push ebx
-	push eax
-	push ecx
-	push edx
+	push rbx
+	push rax
+	push rcx
+	push rdx
 
-	mov ebx, [TEXT]
+	mov ebx, [r10]
 	mov edx, 0
-	mov ecx, 6				;pop six values in case of error (4 from this function, return address and the original stack base pointer)
+	mov ecx, 6				#pop six values in case of error (4 from this function, return address and the original stack base pointer)
 
   start_analyzing_input:
 	cmp BYTE [ebx], 0
 	je quit_analyzing_input
-	cmp BYTE [ebx], 10		;LF
+	cmp BYTE [ebx], 10		#LF
 	je quit_analyzing_input	
-	cmp BYTE [ebx], 13		;CR
+	cmp BYTE [ebx], 13		#CR
 	je quit_analyzing_input	
 
-	mov eax, 2				;code error 2 - wrong characters in the string
+	mov eax, 2				#code error 2 - wrong characters in the string
 
 	cmp BYTE [ebx], 32
 	jl exit
@@ -136,25 +136,25 @@ analyze_input:
 	jmp	start_analyzing_input
 
   quit_analyzing_input:
-	mov eax, 1				;code error 1 - the string is too long
+	mov eax, 1				#code error 1 - the string is too long
 
-	imul edx, 11			;one symbol = 11 pixels
-	add edx, 55				;quiet zone + start symbol + checksum + stop symbol + quiet zone
-	imul edx, [BWTH]		;multiplying by bar width
-	cmp edx, 600			;comparing the image length to maximal length (600 pixels)
+	imul edx, 11			#one symbol = 11 pixels
+	add edx, 55				#quiet zone + start symbol + checksum + stop symbol + quiet zone
+	imul edx, [r9]		#multiplying by bar width
+	cmp edx, 600			#comparing the image length to maximal length (600 pixels)
 	jg exit
 
-	pop edx
-	pop ecx
-	pop eax
-	pop ebx
+	pop rdx
+	pop rcx
+	pop rax
+	pop rbx
 	ret
 
 paint_white:
-	push eax
-	push ebx
+	push rax
+	push rbx
 
-	mov eax, [DEST]
+	mov eax, [r8]
 	add eax, 54
 	mov ebx, eax
 	add ebx, 90000
@@ -164,25 +164,25 @@ paint_white:
 	cmp eax, ebx
 	jl paint_white_loop
 	
-	pop ebx
-	pop eax
+	pop rbx
+	pop rax
 	ret
 
-get_barcode:				;ebx should contain the character index
+get_barcode:				#ebx should contain the character index
 	shl ebx, 3
-	add ebx, CODES
+	add ebx, r12
 	ret
 	
-paint_bar:					;edx should contain the pixel offset from lhs
-	push ebx
-	push edx
+paint_bar:					#edx should contain the pixel offset from lhs
+	push rbx
+	push rdx
 
-	mov ebx, [DEST]
+	mov ebx, [r8]
 	add ebx, 54
 	imul edx, 3
 	add ebx, edx
 	mov edx, ebx
-	add edx, 90000			;edx contains the address of first pixel to paint
+	add edx, 90000			#edx contains the address of first pixel to paint
   paint_loop:
 	mov [ebx], BYTE 0
 	mov [ebx+1], BYTE 0
@@ -191,13 +191,13 @@ paint_bar:					;edx should contain the pixel offset from lhs
 	cmp ebx, edx
 	jl paint_loop
 
-	pop edx
-	pop ebx
+	pop rdx
+	pop rbx
 	ret
 
-paint_char:					;ebx should contain the character code / edx should contain the pixel offset from lhs
-	push eax
-	push ebx
+paint_char:					#ebx should contain the character code / edx should contain the pixel offset from lhs
+	push rax
+	push rbx
 
 	call get_barcode
 
@@ -206,9 +206,9 @@ paint_char:					;ebx should contain the character code / edx should contain the 
 	cmp eax, 0
 	je quit_painting_char
 
-	imul eax, [BWTH]			;al * BWTH (= how many 1-pixel bars to paint) is stored in AX!
+	imul eax, [r9]			#al * r9 (= how many 1-pixel bars to paint) is stored in AX!
 	
-	add eax, edx				;eax contains (the first) offset, which should not be painted
+	add eax, edx				#eax contains (the first) offset, which should not be painted
 
   black_part:
 	call paint_bar
@@ -220,41 +220,41 @@ paint_char:					;ebx should contain the character code / edx should contain the 
 	movzx eax, BYTE [ebx+1]
 	cmp eax, 0
 	je quit_painting_char
-	imul eax, [BWTH]
+	imul eax, [r9]
 	add edx, eax
 	add ebx, 2
 	jmp whole_bar
 
   quit_painting_char:
 
-	pop ebx
-	pop eax
+	pop rbx
+	pop rax
 	ret
 
 generate_barcode:
-	push ebx
-	push eax
-	push ecx
-	push edx
+	push rbx
+	push rax
+	push rcx
+	push rdx
 
-	mov ebx, 104			;START_B symbol
+	mov ebx, 104			#START_B symbol
 	mov edx, 10
-	imul edx, [BWTH]		;offset (quiet zone)
+	imul edx, [r9]		#offset (quiet zone)
 
 	call paint_char
 
 	mov ecx, 0
-	mov eax, 104			;checksum
+	mov eax, 104			#checksum
 
   start_generation:
-	mov ebx, [TEXT]
+	mov ebx, [r10]
 	movzx ebx, BYTE [ebx+ecx]
 
 	cmp ebx, 0
 	je finish_generating
-	cmp ebx, 10		;LF
+	cmp ebx, 10		#LF
 	je finish_generating
-	cmp ebx, 13		;CR
+	cmp ebx, 13		#CR
 	je finish_generating
 
 	sub ebx, 32
@@ -263,14 +263,14 @@ generate_barcode:
 
 	inc ecx
 	imul ebx, ecx
-	add eax, ebx		;update checksum
+	add eax, ebx		#update checksum
 
 	jmp	start_generation
 
   finish_generating:
 
-	mov bl, 103			;ebx will be reassigned in a moment, it can hold the divider
-	div bl				;checksum = reminder of eax/103
+	mov bl, 103			#ebx will be reassigned in a moment, it can hold the divider
+	div bl				#checksum = reminder of eax/103
 	movzx ebx, ah
 	
 	call paint_char
@@ -278,8 +278,8 @@ generate_barcode:
 	mov ebx, 106
 	call paint_char
 
-	pop edx
-	pop ecx
-	pop eax
-	pop ebx
+	pop rdx
+	pop rcx
+	pop rax
+	pop rbx
 	ret
--- makefile
+++ makefile
@@ -1,15 +1,16 @@
 CC=g++
-ASMBIN=nasm
+ASMBIN=as
 
 all : asm cc link
 asm : 
-	$(ASMBIN) -o encode128.o -f elf -g -l encode128.lst encode128.asm
+	$(ASMBIN) -msyntax=intel -mnaked-reg -o encode128.o -g encode128.asm
 cc :
-	$(CC) -m32 -c -g -O0 main.c &> errors.txt
+	$(CC) -c -g -O0 main.c &> errors.txt
 link :
-	$(CC) -m32 -g -o encode128 main.o encode128.o
+	$(CC) -g -o encode128 main.o encode128.o
 clean :
 	rm *.o
 	rm encode128
 	rm errors.txt	
 	rm encode128.lst
+
